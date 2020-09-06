6aa48f9dcce982cbc43c91b09dddda72a53cd210
chewjta <chewjta@gmail.com>
2020-09-06 23:55:43 +0800

    bootstrap done for questions overview

--- views/questions/allquestions.jsx
+++ views/questions/allquestions.jsx
@@ -20,34 +20,45 @@ export default class Allquestions extends React.Component {
 
 
     let answeredList = answered.map(item=>{
-        return <li><a href={`/questions/${item.id}`}>{item.question}</a></li>
+        return <li className="list-group-item font-weight-lighter"><a href={`/questions/${item.id}`}>{item.question}</a></li>
     })
 
     let unansweredList = unanswered.map(item=>{
-        return <li><a href={`/questions/${item.id}`}>{item.question}</a></li>
+        return <li className="list-group-item font-weight-lighter"><a href={`/questions/${item.id}`}>{item.question}</a></li>
     })
 
 
 
     return (
       <html>
-        <head />
+        <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
+        </head>
         <body>
-          <h3 id='header'>Current Questions</h3>
-          <h5>Unanswered Questions:</h5>
-          <ul>{unansweredList}</ul>
-          <h5>Answered Questions:</h5>
-          <ul>{answeredList}</ul>
-
-
-        <form id='newquestion' method='GET' action='/questions/new'>
-            <input type='submit' value='Add a new question'/>
+<h2 className="text-center font-weight-lighter">Question Bank</h2>
+<div class="card text-center h-100" style={{marginTop:'20px'}}>
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Answered Questions</h5>
+    <p class="card-text">{answeredList}</p>
+  </div>
+  <div class="card-footer text-muted">
+    <div class="card-body">
+    <h5 class="card-title">Unanswered Questions</h5>
+    <p class="card-text">{unansweredList}</p>
+    <form id='newquestion' method='GET' action='/questions/new'>
+            <input type='submit' value='Add a new question' className="btn btn-primary"/>
           </form>
-          <br /> <br />
-<form id='newquestion' method='GET' action='/dashboard'>
-            <input type='submit' value='Back to home'/>
+<br />
+          <form id='newquestion' method='GET' action='/dashboard'>
+            <input type='submit' value='Back to home' className="btn btn-info"/>
           </form>
 
+  </div>
+  </div>
+</div>
         </body>
       </html>
       );
