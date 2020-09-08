bba971177d4199c794d6bbb2065ad27d996587c4
chewjta <chewjta@gmail.com>
2020-09-09 01:32:28 +0800

    asdasf

--- controllers/students.js
+++ controllers/students.js
@@ -3,12 +3,15 @@ module.exports = (db) => {
 let studentDashboard = (request,response) => {
         let {username,userId,type,logIn} = request.cookies;
         db.students.getStudentDashboard((err,result)=>{
+            if(err){console.log(err)}else {
             if(logIn && type =='student'){
             response.render('students/dashboard',{username, userId, data:result.rows})
         } else {
             response.redirect('/accounts/login')
         }
+    }
         })
+
     }
 
 
